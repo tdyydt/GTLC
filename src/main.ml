e758a24f42c34a2124bbe987749c957bda5b84c1
Yuya Tsuda <yytsuda@gmail.com>
2018-04-01 16:38:14 +0900

    Add translate part to main

--- src/main.ml
+++ src/main.ml
@@ -1,16 +1,23 @@
+open Printf
 open Syntax
 open Typing
+open Translate
 
+(* TODO: debug = true の時のみ，途中経過を出力 *)
 let read_ty_print () =
   print_string "# ";
   flush stdout;
+  let gamma = Environment.empty in
   try
     let e = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
-    let t = ty_exp Environment.empty e in
-    Printf.printf "val %s : %s"
-      "-"
-      (string_of_ty t);
-    print_newline ()
+    let t = ty_exp gamma e in
+    printf "val %s : %s" "-" (string_of_ty t);
+    print_newline ();
+    let f, t' = translate_exp gamma e in
+    print_string @@ C.string_of_exp f;
+    print_newline ();
+    (* TODO: assert t = t' *)
+    print_string @@ string_of_bool (t = t');
   with
   | e -> raise e
 
