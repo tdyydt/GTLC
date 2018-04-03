d85c4488b01709d560e7de8ba8130eae1a5307df
Yuya Tsuda <yytsuda@gmail.com>
2018-04-03 23:05:48 +0900

    Writing eval (cast)

--- src/eval.ml
+++ src/eval.ml
@@ -7,7 +7,13 @@ open Printf
 (* TODO: small-step operational semantics
  * その場合，関数 step など，sf にある *)
 
-(* subset of C.exp *)
+(* TagInt, TagBool, TagFun でいいのでは？ *)
+type tag =
+  | I                           (* int *)
+  | B                           (* bool *)
+  | F                           (* Function (Arrow) [?->?] *)
+
+(* exval, dnval *)
 type value =
   | IntV of int
   | BoolV of bool
@@ -16,7 +22,7 @@ type value =
   | FunV of id * exp * value Environment.t
   (* tagged value, or injection
    * cast from Ground to TyDyn *)
-  | Tagged
+  | Tagged of tag * value
 
 (* Big-step evaluation ? *)
 let rec eval_exp env = function
@@ -59,4 +65,38 @@ let rec eval_exp env = function
          (* [(fun (x:_) -> body) v2] *)
          eval_exp (Environment.add x v2 fun_env) body
       | _ -> err "eval App: Non-function value is applied")
-  | CastExp (f, t1, t2) -> todo "Cast"
+  | CastExp (f, t1, t2) ->
+     let v = eval_exp env f in
+     (match t1, t2 with
+      (* IdBase *)
+      | TyInt, TyInt -> v
+      | TyBool, TyBool -> v
+      (* IdStar *)
+      | TyDyn, TyDyn -> v
+      (* Ground *)
+      (* 動的型へのキャストの際に，タグを付ける(キャスト前の型の情報を表す) *)
+      | TyInt, TyDyn -> Tagged (I, v)
+      | TyBool, TyDyn -> Tagged (B, v)
+      | TyFun _, TyDyn -> (* Tagged (F, *) todo ""
+      (* Expand *)
+
+      (* Succeed (Collapse), Fail (Conflict) *)
+      | TyDyn, TyInt ->
+         (match v with
+          (* [v': int => ? => int]
+           * int のタグが付いていたならば ok *)
+          | Tagged (I, v') -> v'
+          | Tagged (_, _) -> err "Blame: Fail int"
+          | _ -> err "Should not happen: Untagged value")
+      | TyDyn, TyBool ->
+         (match v with
+          | Tagged (B, v') -> v'
+          | Tagged (_, _) -> err "Blame: Fail bool"
+          | _ -> err "Should not happen: Untagged value")
+      | TyDyn, TyFun (TyDyn, TyDyn) ->
+         (match v with
+          | Tagged (F, v') -> v'
+          | Tagged (_, _) -> err "Blame: Fail fun"
+          | _ -> err "Should not happen: Untagged value")
+
+      (* AppCast (Wrap) *)
