ccdb53c4b2c01b800b92af87937d956805066c51
Yuya Tsuda <yytsuda@gmail.com>
2018-05-03 12:39:23 +0900

    Improve string_of_* with consideration of precedence
    though still experimental.
    Add with_paren, prec_*

--- src/syntax.ml
+++ src/syntax.ml
@@ -9,15 +9,24 @@ type ty =
   | TyFun of ty * ty
   | TyDyn                       (* the dynamic type ? *)
 
+let with_paren flag s =
+  if flag then "(" ^ s ^ ")" else s
+
+(* precedence of type *)
+(* larger number means higher precedence *)
+let prec_ty = function
+  | (TyInt | TyBool | TyDyn) -> 2
+  | TyFun _ -> 1
+
+(* t1 > t2 : t1 associates stronger than t2 *)
+let gt_ty t1 t2 = (prec_ty t1) > (prec_ty t2)
+
 let rec string_of_ty = function
   | TyInt -> "int"
   | TyBool -> "bool"
-  | TyFun (t1,t2) ->
-     (* If t1 is TyFun, put it in parentheses. *)
-     let str1 = match t1 with
-       | TyFun (_,_) as t1 -> sprintf "(%s)" (string_of_ty t1)
-       | _ as t1 -> string_of_ty t1 in
-     sprintf "%s -> %s" str1 (string_of_ty t2)
+  | TyFun (t1,t2) as t ->
+     sprintf "%s -> %s"
+       (with_paren (gt_ty t t1) (string_of_ty t1)) (string_of_ty t2)
   | TyDyn -> "?"
 
 type binOp = Plus | Minus | Mult | Div | Lt | Gt | Eq | LE | GE | LAnd | LOr
@@ -34,6 +43,14 @@ let string_of_binop = function
   | GE    -> ">="
   | LAnd  -> "&&"
   | LOr   -> "||"
+
+(* 1~10 で返す *)
+let prec_binop = function
+  | LOr -> 1
+  | LAnd -> 2
+  | Lt | Gt | Eq | LE | GE -> 3
+  | Plus | Minus -> 4
+  | Mult | Div -> 5
 
 (* Gradually typed surface language *)
 module G = struct
@@ -52,29 +69,6 @@ module G = struct
      * [let rec f (x:t1) : t2 = e1 in e2]
      * where t2 is return type annotation *)
     | LetRecExp of id * id * ty * ty * exp * exp
-
-  let rec string_of_exp = function
-    | Var x -> x
-    | ILit n -> string_of_int n
-    | BLit b -> string_of_bool b
-    | BinOp (op, e1, e2) ->
-       sprintf "(%s %s %s)"
-         (string_of_exp e1) (string_of_binop op) (string_of_exp e2)
-    | IfExp (e1, e2, e3) ->
-       sprintf "(if %s then %s else %s)"
-         (string_of_exp e1) (string_of_exp e2) (string_of_exp e3)
-    | LetExp (x, e1, e2) ->
-       sprintf "(let %s = %s in %s)"
-         x (string_of_exp e1) (string_of_exp e2)
-    | FunExp (x, t, f) ->
-       sprintf "(fun (%s : %s) -> %s)"
-         x (string_of_ty t) (string_of_exp f)
-    | AppExp (e1, e2) ->
-       sprintf "(%s %s)" (string_of_exp e1) (string_of_exp e2)
-    | LetRecExp (x, y, t1, t2, e1, e2) ->
-       sprintf "(let rec %s (%s : %s) : %s = %s in %s)"
-         x y (string_of_ty t1) (string_of_ty t2)
-         (string_of_exp e1) (string_of_exp e2)
 
   type program =
     | Exp of exp
@@ -99,37 +93,61 @@ module C = struct
     (* CastExp(f,t1,t2) ==> [f: t1 => t2]  *)
     | CastExp of exp * ty * ty
 
-  (* TODO: reduce parentheses, printer *)
-  let rec string_of_exp = function
-    | Var x -> x
-    | ILit n -> string_of_int n
-    | BLit b -> string_of_bool b
-    | BinOp (op, f1, f2) ->
-       sprintf "(%s %s %s)"
-         (string_of_exp f1) (string_of_binop op) (string_of_exp f2)
-    | IfExp (f1, f2, f3) ->
-       sprintf "(if %s then %s else %s)"
-         (string_of_exp f1) (string_of_exp f2) (string_of_exp f3)
-    | LetExp (x, f1, f2) ->
-       sprintf "(let %s = %s in %s)"
-         x (string_of_exp f1) (string_of_exp f2)
-    | FunExp (x, t, f) ->
-       sprintf "(fun (%s : %s) -> %s)"
-         x (string_of_ty t) (string_of_exp f)
-    | AppExp (f1, f2) ->
-       sprintf "(%s %s)" (string_of_exp f1) (string_of_exp f2)
-    | LetRecExp (x, y, t1, t2, f1, f2) ->
-       sprintf "(let rec %s (%s : %s) : %s = %s in %s)"
-         x y (string_of_ty t1) (string_of_ty t2)
-         (string_of_exp f1) (string_of_exp f2)
-    | CastExp (f, t1, t2) ->
-       sprintf "(%s : %s => %s)"
-         (string_of_exp f) (string_of_ty t1) (string_of_ty t2)
-
   type program =
     | Exp of exp
     | LetDecl of id * exp
     | LetRecDecl of id * id * ty * ty * exp
+
+  (* precedence of expression *)
+  let prec_exp = function
+    | LetExp _ | LetRecExp _ | FunExp _ -> 10
+    | IfExp _ -> 20
+    | CastExp _ -> 21           (* ?????? *)
+    | BinOp (op, _, _) -> 30 + prec_binop op
+    | AppExp _ -> 40
+    | Var _ | ILit _ | BLit _ -> 50
+
+  (* f1 > f2 : f1 associates stronger than f2 *)
+  let gt_exp f1 f2 = (prec_exp f1) > (prec_exp f2)
+  (* f1 >= f2 *)
+  let ge_exp f1 f2 = (prec_exp f1) >= (prec_exp f2)
+
+  let rec string_of_exp = function
+    | Var x -> x
+    | ILit n -> string_of_int n
+    | BLit b -> string_of_bool b
+    | BinOp (op, f1, f2) as f ->
+       (* Left assoc *)
+       sprintf "%s %s %s"
+         (with_paren (gt_exp f f1) (string_of_exp f1)) (string_of_binop op)
+         (with_paren (ge_exp f f2) (string_of_exp f2))
+    | IfExp (f1, f2, f3) as f ->
+       (* f1,f2 は If 等ならカッコが要る *)
+       (* f3 にカッコは不要 *)
+       sprintf "if %s then %s else %s"
+         (with_paren (ge_exp f f1) (string_of_exp f1))
+         (with_paren (ge_exp f f2) (string_of_exp f2))
+         (string_of_exp f3)
+    | LetExp (x, f1, f2) as f ->
+       sprintf "let %s = %s in %s"
+         x (with_paren (ge_exp f f1) (string_of_exp f1))
+         (string_of_exp f2)
+    | FunExp (x, t, f1) ->
+       sprintf "fun (%s : %s) -> %s"
+         x (string_of_ty t) (string_of_exp f1)
+    | AppExp (f1, f2) as f ->
+       sprintf "%s %s"
+         (with_paren (ge_exp f f1) (string_of_exp f1))
+         (with_paren (gt_exp f f2) (string_of_exp f2))
+    | LetRecExp (x, y, t1, t2, f1, f2) as f ->
+       sprintf "let rec %s (%s : %s) : %s = %s in %s"
+         x y (string_of_ty t1) (string_of_ty t2)
+         (with_paren (ge_exp f f1) (string_of_exp f1))
+         (string_of_exp f2)
+    | CastExp (f1, t1, t2) as f ->
+       sprintf "%s : %s => %s"
+         (with_paren (ge_exp f f1) (string_of_exp f1))
+         (string_of_ty t1) (string_of_ty t2)
 
   let rec string_of_program = function
     | Exp f -> string_of_exp f
