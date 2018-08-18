55c3374326871748b1e2b8aa16c68bfa938f216f
Yuya Tsuda <yytsuda@gmail.com>
2018-08-19 00:24:02 +0900

    Update error handling

--- src/main.ml
+++ src/main.ml
@@ -9,17 +9,17 @@ open Stringify
 let rec read_eval_print gamma env =
   print_string "# ";
   flush stdout;
+  let lexbuf = Lexing.from_channel stdin in
   try
     (* p : Syntax.G.program *)
-    let p = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
+    let p = Parser.toplevel Lexer.main lexbuf in
     print_string "[Typing]\n";
     let (gamma', ty_bindings) = ty_prog gamma p in
     List.iter (fun (x,t) ->
-        printf "val %s : %s" x (string_of_ty t);
-        print_newline ())
+        printf "val %s : %s\n" x (string_of_ty t))
       ty_bindings;
 
-    (* Cast Insertion Translation *)
+    (* Cast-Insertion Translation *)
     print_string "[Translation]\n";
     (* q : Syntax.C.program *)
     let (q, ty_bindings') = translate_prog gamma p in
@@ -34,31 +34,28 @@ let rec read_eval_print gamma env =
     let (env', val_bindings) = eval_prog env q in
     List.iter2 (fun (x,t) (x',v) ->
         assert (x' = x);
-        printf "val %s : %s = %s" x (string_of_ty t) (string_of_value v);
-        print_newline ())
+        printf "val %s : %s = %s\n"
+          x (string_of_ty t) (string_of_value v))
       ty_bindings val_bindings;
 
     read_eval_print gamma' env'
   with
+  (* e.g. Failure("lexing: empty token") *)
+  | Failure m -> printf "Failure: %s\n" m;
+                 read_eval_print gamma env
+  | Parser.Error ->             (* Menhir *)
+     let token = Lexing.lexeme lexbuf in
+     printf "Parser.Error: unexpected token: %s\n" token;
+     Lexing.flush_input lexbuf;
+     read_eval_print gamma env
   | Syntax_error s | Typing_error s | CI_error s | Eval_error s ->
      print_string s;
      print_newline ();
      read_eval_print gamma env
   | Blame (tag1, tag2) ->
-     printf "Blame: %s is incompatible with tag %s"
+     printf "Blame: %s is incompatible with tag %s\n"
        (string_of_tag tag2) (string_of_tag tag1);
-     print_newline ();
      read_eval_print gamma env
-  (* TODO: Get error message by Menhir? *)
-  | Parsing.Parse_error ->      (* Menhir *)
-     print_string "Parse_error.";
-     print_newline ();
-     read_eval_print gamma env
-  (* Lexer error *)
-  (* e.g. Failure("lexing: empty token") *)
-  | Failure m -> print_string m;
-                 print_newline ();
-                 read_eval_print gamma env
 
 let initial_gamma = Environment.empty
 let initial_env = Environment.empty
