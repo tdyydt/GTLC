fe1199eb4953f74f9e339d0e7f4715da6509ed3a
Yuya Tsuda <yytsuda@gmail.com>
2018-09-06 21:46:29 +0900

    Refactor: Use Format.printf

--- src/main.ml
+++ src/main.ml
@@ -6,6 +6,8 @@ open Typing
 open Translate
 open Eval
 
+(* let printf fmt = fprintf std_formatter fmt *)
+
 let rec read_eval_print gamma env =
   printf "# @?";                (* @? to flush the output buffer *)
   flush stdout;
@@ -13,75 +15,75 @@ let rec read_eval_print gamma env =
   try
     let open Syntax in
     let p : G.program = Parser.toplevel Lexer.main lexbuf in
-    fprintf std_formatter "*** Typing ***\n";
+    printf "*** Typing ***\n";
     let open Typing in
     let (gamma', ty_bindings) = G.ty_prog gamma p in
     let open Pp in
     List.iter (fun (x,t) ->
-        fprintf std_formatter "val %s : %a\n" x pp_ty t)
+        printf "val %s : %a\n" x pp_ty t)
       ty_bindings;
 
     (* Cast-insertion Translation *)
-    print_string "*** Cast Insertion ***\n";
+    printf "*** Cast Insertion ***\n";
     let open Syntax in
     let ((q : C.program), ty_bindings') = translate_prog gamma p in
     (* check soundness ; equality of lists *)
     assert (ty_bindings = ty_bindings');
     (* Show result of translation *)
     let open Pp in
-    fprintf std_formatter "%a\n" C.pp_prog q;
+    printf "%a\n" C.pp_prog q;
 
     (* Eval *)
-    fprintf std_formatter "*** Evaluation ***\n";
+    printf "*** Evaluation ***\n";
     let (env', val_bindings) = eval_prog env q in
     List.iter2 (fun (x,t) (x',v) ->
         assert (x' = x);
-        fprintf std_formatter "val %s : %a = %a\n"
+        printf "val %s : %a = %a\n"
           x pp_ty t pp_value v)
       ty_bindings val_bindings;
 
     read_eval_print gamma' env'
   with
   (* e.g. Failure("lexing: empty token") *)
-  | Failure m -> fprintf std_formatter "Failure: %s\n" m;
+  | Failure m -> printf "Failure: %s\n" m;
                  read_eval_print gamma env
   | Parser.Error ->             (* Menhir *)
      let token = Lexing.lexeme lexbuf in
-     fprintf std_formatter "Parser.Error: unexpected token: %s\n" token;
+     printf "Parser.Error: unexpected token: %s\n" token;
      Lexing.flush_input lexbuf;
      read_eval_print gamma env
   | Syntax_error (r, msg) ->
-     fprintf std_formatter "%a\n" print_range r;
-     fprintf std_formatter "%s\n" msg;
+     printf "%a\n" print_range r;
+     printf "%s\n" msg;
      read_eval_print gamma env
 
   | Type_error0 (r, msg) ->
-     fprintf std_formatter "%a\n" print_range r;
-     fprintf std_formatter "%s\n" msg;
+     printf "%a\n" print_range r;
+     printf "%s\n" msg;
      read_eval_print gamma env
   | Type_error1 (r, fmt, t) ->
      let open Pp in
-     fprintf std_formatter "%a\n" print_range r;
-     fprintf std_formatter (fmt ^^ "\n") pp_ty t;
+     printf "%a\n" print_range r;
+     printf (fmt ^^ "\n") pp_ty t;
      read_eval_print gamma env
   | Type_error2 (r, fmt, t1, t2) ->
      let open Pp in
-     fprintf std_formatter "%a\n" print_range r;
-     fprintf std_formatter (fmt ^^ "\n") pp_ty t1 pp_ty t2;
+     printf "%a\n" print_range r;
+     printf (fmt ^^ "\n") pp_ty t1 pp_ty t2;
      read_eval_print gamma env
 
   | Blame (r, plr, tag1, tag2) ->
-     fprintf std_formatter "%a\n" print_range r;
+     printf "%a\n" print_range r;
      begin match plr with
-     | Pos -> fprintf std_formatter "Blame on the expression side: %s => %s\n"
+     | Pos -> printf "Blame on the expression side: %s => %s\n"
                 (string_of_tag tag1) (string_of_tag tag2)
-     | Neg -> fprintf std_formatter "Blame on the environment side: %s => %s\n"
+     | Neg -> printf "Blame on the environment side: %s => %s\n"
                 (string_of_tag tag1) (string_of_tag tag2)
      end;
      read_eval_print gamma env
   (* Fatal errors, or implementation bugs *)
   | CI_bug s | Eval_bug s ->
-     fprintf std_formatter "%s\n" s;
+     printf "%s\n" s;
      exit 1
 
 let initial_gamma = Environment.empty
